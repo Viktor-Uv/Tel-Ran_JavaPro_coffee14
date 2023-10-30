a48914bbb618072dcd80953019444453ef2c421a
Viktor-Uv <sev-vic@yandex.com>
2023-10-30 21:28:57 +0100

    Initial commit

---
+++ src/test/java/com/example/coffee14/Coffee14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffee14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coffee14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
